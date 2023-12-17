e06eda91fac67b42e5e630bb1bafe9cdfe030531
AngelMelendres <fabian_melendres@hotmail.com>
2023-12-16 19:15:47 -0500

    actualizacion base datos

--- backend/app/Http/Controllers/EvaluaciondesempenoController.php
+++ backend/app/Http/Controllers/EvaluaciondesempenoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Empleado;
 use Illuminate\Http\Request;
 use App\Models\EvaluacionDesempeno;
 use Illuminate\Support\Facades\Validator;
@@ -123,15 +124,12 @@ class EvaluacionDesempenoController extends Controller
         // Actualizar la evaluación de desempeño sin el archivo
         $evaluacionDesempeno->update($request->except('archivo'));
 
-        // Subir el archivo al directorio público
+        // Subir y actualizar el archivo si existe en la solicitud
         if ($request->hasFile('archivo')) {
             $archivo = $request->file('archivo');
-            $rutaArchivo = $archivo->storeAs('public/archivos/evaluaciones', 'evaluacion_' . $evaluacionDesempeno->idEvaluacionDesempeno . '.' . $archivo->getClientOriginalExtension());
+            $rutaArchivo = $archivo->storeAs('archivos/evaluaciones', 'evaluacion_' . $evaluacionDesempeno->idEvaluacionDesempeno . '.' . $archivo->getClientOriginalExtension());
 
-            // Obtener la ruta relativa del archivo
-            $rutaArchivo = str_replace('public/', 'storage/', $rutaArchivo);
-
-            // Actualizar la evaluación de desempeño con la ruta del archivo
+            // Actualizar la evaluación de desempeño con la nueva ruta del archivo
             $evaluacionDesempeno->update(['archivo' => $rutaArchivo]);
         }
 
@@ -155,5 +153,179 @@ class EvaluacionDesempenoController extends Controller
         $evaluacionDesempeno->delete();
 
         return response()->json(['successful' => true, 'message' => 'Evaluación de Desempeño eliminada correctamente']);
+    }
+
+
+
+
+
+
+
+
+    /**
+     * Obtener todas las evaluaciones para un empleado específico.
+     *
+     * @param  int  $idEmpleado - ID del empleado
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function obtenerEvaluacionesPorEmpleado($idEmpleado)
+    {
+        // Verificar si el empleado existe
+        $empleado = Empleado::find($idEmpleado);
+
+        if (!$empleado) {
+            return response()->json(['successful' => false, 'error' => 'Empleado no encontrado'], 404);
+        }
+
+        // Obtener las evaluaciones del empleado
+        $evaluaciones = EvaluacionDesempeno::where('idEmpleado', $idEmpleado)->get();
+
+        return response()->json(['successful' => true, 'data' => $evaluaciones]);
+    }
+
+    /**
+     * Obtener todas las evaluaciones realizadas por un evaluador específico.
+     *
+     * @param  int  $idEvaluador - ID del evaluador
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function obtenerEvaluacionesPorEvaluador($idEvaluador)
+    {
+        // Verificar si el evaluador existe
+        $evaluador = Empleado::find($idEvaluador);
+
+        if (!$evaluador) {
+            return response()->json(['successful' => false, 'error' => 'Evaluador no encontrado'], 404);
+        }
+
+        // Obtener las evaluaciones del evaluador
+        $evaluaciones = EvaluacionDesempeno::where('idEvaluador', $idEvaluador)->get();
+
+        return response()->json(['successful' => true, 'data' => $evaluaciones]);
+    }
+
+
+    /**
+     * Obtener evaluaciones por fecha.
+     *
+     * @param  string  $fechaInicio
+     * @param  string  $fechaFin
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function obtenerEvaluacionesPorFecha($fechaInicio, $fechaFin)
+    {
+        $validator = Validator::make(compact('fechaInicio', 'fechaFin'), [
+            'fechaInicio' => 'required|date',
+            'fechaFin' => 'required|date|after_or_equal:fechaInicio',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['successful' => false, 'errors' => $validator->errors()], 422);
+        }
+
+        $evaluaciones = EvaluacionDesempeno::whereBetween('fechaEvaluacion', [$fechaInicio, $fechaFin])->get();
+
+        return response()->json(['successful' => true, 'data' => $evaluaciones]);
+    }
+
+
+
+    /**
+     * Obtener evaluaciones con una calificación general superior a un valor específico.
+     *
+     * @param  float  $calificacionMinima - Calificación mínima requerida
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function obtenerEvaluacionesPorCalificacion($calificacionMinima)
+    {
+        $validator = Validator::make(['calificacionMinima' => $calificacionMinima], [
+            'calificacionMinima' => 'required|numeric|min:0|max:100',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['successful' => false, 'errors' => $validator->errors()], 422);
+        }
+
+        $evaluaciones = EvaluacionDesempeno::where('calificacionGeneral', '>', $calificacionMinima)->get();
+        return response()->json(['successful' => true, 'data' => $evaluaciones]);
+    }
+
+    /**
+     * Contar el número total de evaluaciones por empleado.
+     *
+     * @param  int  $idEmpleado - ID del empleado
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function contarEvaluacionesPorEmpleado($idEmpleado)
+    {
+        // Validar que $idEmpleado sea un número entero
+        $validator = Validator::make(['idEmpleado' => $idEmpleado], [
+            'idEmpleado' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['successful' => false, 'errors' => $validator->errors()], 422);
+        }
+
+        $empleado = Empleado::find($idEmpleado);
+
+        if (!$empleado) {
+            return response()->json(['successful' => false, 'error' => 'Empleado no encontrado'], 404);
+        }
+
+        // Contar las evaluaciones del empleado
+        $cantidadEvaluaciones = EvaluacionDesempeno::where('idEmpleado', $idEmpleado)->count();
+        return response()->json(['successful' => true, 'cantidad evaluaciones' => $cantidadEvaluaciones]);
+    }
+
+    /**
+     * Calcular el promedio de calificación general para un evaluador específico.
+     *
+     * @param  int  $idEvaluador - ID del evaluador
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function calcularPromedioCalificacionPorEvaluador($idEvaluador)
+    {
+        // Validar que $idEvaluador sea un número entero
+        $validator = Validator::make(['idEvaluador' => $idEvaluador], [
+            'idEvaluador' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['successful' => false, 'errors' => $validator->errors()], 422);
+        }
+
+        // Verificar si el evaluador existe
+        $evaluador = Empleado::find($idEvaluador);
+
+        if (!$evaluador) {
+            return response()->json(['successful' => false, 'error' => 'Evaluador no encontrado'], 404);
+        }
+
+        // Calcular el promedio de calificación del evaluador
+        $promedioCalificacion = EvaluacionDesempeno::where('idEvaluador', $idEvaluador)->avg('calificacionGeneral');
+        return response()->json(['successful' => true, 'data' => $promedioCalificacion]);
+    }
+
+
+    /**
+     * Obtener evaluaciones por estado.
+     *
+     * @param  string  $estado
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function listasEvaluacionesPorEstado($estado)
+    {
+        $validator = Validator::make(['estado' => $estado], [
+            'estado' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['successful' => false, 'errors' => $validator->errors()], 422);
+        }
+
+        $evaluaciones = EvaluacionDesempeno::where('estadoEvaluacion', $estado)->get();
+
+        return response()->json(['successful' => true, 'data' => $evaluaciones]);
     }
 }
