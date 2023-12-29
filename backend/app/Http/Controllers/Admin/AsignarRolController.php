4a3b53003106439b9e6cec37541994d7d322e2b6
EdisonGualle <ed.gualle@gmail.com>
2023-12-29 15:37:16 -0500

    Implementación de busqueda de usuarios con rol v1.0
    Se lista todos los usuarios que tienen un ro.l
    se lista los usuarios por rol en especifico .

--- backend/app/Http/Controllers/Admin/AsignarRolController.php
+++ backend/app/Http/Controllers/Admin/AsignarRolController.php
@@ -56,19 +56,19 @@ class AsignarRolController extends Controller
         return response()->json(['message' => 'Rol actualizado con éxito']);
     }
 
-    // Eliminar
-    public function eliminarRolUsuario(Request $request)
+    // Eliminar rol de usuario
+    public function eliminarRolUsuario($usuario)
     {
         // Obtener el usuario autenticado con Laravel Sanctum
         $usuarioAutenticado = auth()->user();
 
         // Validar que el usuario autenticado no sea el mismo que se está modificando
-        if ($usuarioAutenticado && $usuarioAutenticado->usuario === $request->input('usuario')) {
+        if ($usuarioAutenticado && $usuarioAutenticado->usuario === $usuario) {
             return response()->json(['error' => 'No puedes eliminar tu propio rol'], 400);
         }
 
         // Validar la existencia del usuario
-        $usuario = User::where('usuario', $request->input('usuario'))->first();
+        $usuario = User::where('usuario', $usuario)->first();
 
         if (!$usuario) {
             return response()->json(['error' => 'Usuario no encontrado'], 404);
@@ -77,19 +77,75 @@ class AsignarRolController extends Controller
         // Obtener rol antes de eliminar
         $rolesAntesDeEliminar = $usuario->getRoleNames();
 
-        // Eliminar  usuario
+        // Eliminar usuario
         $usuario->syncRoles([]);
 
         // Obtener roles después de eliminar
-        $rolesDespuesDeEliminar = $usuario->getRoleNames();
+        $rolesDespuésDeEliminar = $usuario->getRoleNames();
 
         // Verificar si el usuario ya no tiene ningún rol
-        if ($rolesAntesDeEliminar->isEmpty() && $rolesDespuesDeEliminar->isEmpty()) {
-            return response()->json(['error' => 'El usuario no tiene ningun rol'], 404);
+        if ($rolesAntesDeEliminar->isEmpty() && $rolesDespuésDeEliminar->isEmpty()) {
+            return response()->json(['error' => 'El usuario no tiene ningún rol'], 404);
         }
 
         return response()->json(['message' => 'Rol eliminado con éxito']);
     }
+
+    public function listarUsuariosConRoles()
+    {
+        // Obtén todos los roles
+        $roles = Rol::all();
+    
+        // Inicializa un array para almacenar usuarios con al menos un rol
+        $usersWithRoles = [];
+    
+        // Itera sobre cada rol
+        foreach ($roles as $role) {
+            // Obtén todos los usuarios asociados a este rol
+            $users = User::role($role->name)->get();
+    
+            // Agrega los usuarios al array general
+            $usersWithRoles = array_merge($usersWithRoles, $users->all());
+        }
+
+        // Prepara la respuesta
+        $response = [];
+    
+        // Itera sobre los usuarios con roles y crea la respuesta deseada
+        foreach ($usersWithRoles as $user) {
+            $response[] = [
+                'idUsuario' => $user->idUsuario,
+                'usuario' => $user->usuario, 
+                'rol' => $user->roles->pluck('name')->first(),
+            ];
+        }
+    
+        // Ahora $response contiene la información requerida
+        return response()->json(['success' => true, 'data' => $response], 200);
+    }
+
+
+    // Listar todos los usuarios con rol
+    public function listarUsuariosPorRol($rol)
+    {
+        // Validar la existencia del rol
+        if (!$this->existeRol($rol)) {
+            return response()->json(['error' => 'El rol no existe'], 404);
+        }
+
+        // Obtener todos los usuarios con el rol especificado
+        $usuariosConRol = User::role($rol)->get();
+
+        // Verificar si se encontraron usuarios
+        if ($usuariosConRol->isEmpty()) {
+            return response()->json(['message' => 'No se encontraron usuarios con el rol especificado'], 200);
+        }
+
+        // Devolver la información de los usuarios
+        return response()->json(['usuarios' => $usuariosConRol]);
+    }
+
+
     // Función para verificar si el usuario está activo
     private function isUserActive($user)
     {
--- backend/routes/superAdministrador.php
+++ backend/routes/superAdministrador.php
@@ -6,11 +6,14 @@ use App\Http\Controllers\Config\ConfiguracionController;
 use App\Http\Controllers\Admin\AsignarRolController;
 
 
-//Asignar Roles Routes
+// ASIGNAR ROL routes
+// Asignar rol de determinado usuario  -  los parametros se reciben por Request $request
 Route::post('/asignar-rol', [AsignarRolController::class, 'asignarRolUsuario']);
-Route::delete('/eliminar-rol', [AsignarRolController::class, 'eliminarRolUsuario']);
-
-
+Route::delete('/eliminar-rol/{usuario}', [AsignarRolController::class, 'eliminarRolUsuario']);
+// Listar todos los usuarios con roles
+Route::get('/usuarios-rol', [AsignarRolController::class, 'listarUsuariosConRoles']);
+// Listar usuarios por rol -
+Route::get('/usuarios/{rol}', [AsignarRolController::class, 'listarUsuariosPorRol']);
 
 //CONFIGURACION routes
 Route::get('/configuraciones', [ConfiguracionController::class, 'obtenerConfiguraciones']);
