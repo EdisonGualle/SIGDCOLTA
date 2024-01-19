ab8b0ea57ee8013ab602cde7dc31c4ed20b8353f
AngelMelendres <fabian_melendres@hotmail.com>
2024-01-18 19:41:35 -0500

    act

--- Frontend/src/Pages/Empleados.jsx
+++ Frontend/src/Pages/Empleados.jsx
@@ -1,53 +1,51 @@
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-quartz.css";
-import { AgGridReact } from "ag-grid-react";
-import React, { useEffect, useMemo, useState } from "react";
+import React from "react";
+import {
+  CDataTable,
+  CBadge,
+} from "@coreui/react";
 import useEmpleados from "../hooks/useEmpleados";
 
 const Empleados = () => {
   const { empleados } = useEmpleados();
-  const [rowData, setRowData] = useState([]);
 
-  // Column Definitions: Defines & controls grid columns.
-  const [colDefs] = useState([
-    { headerName: "ID Empleado", field: "idEmpleado", filter: true },
-    { headerName: "Cedula", field: "cedula" },
-    { headerName: "Primer Nombre", field: "primerNombre" },
-    { headerName: "Segundo Nombre", field: "segundoNombre" },
-    { headerName: "Primer Apellido", field: "primerApellido" },
-    { headerName: "Segundo Apellido", field: "segundoApellido" },
-    { headerName: "Fecha de Nacimiento", field: "fechaNacimiento" },
-    { headerName: "Genero", field: "genero" },
-    { headerName: "Teléfono Personal", field: "telefonoPersonal" },
-    { headerName: "Teléfono Trabajo", field: "telefonoTrabajo" },
-    { headerName: "Correo", field: "correo" },
-    { headerName: "Etnia", field: "etnia" },
-    { headerName: "Estado Civil", field: "estadoCivil" },
-    { headerName: "Tipo de Sangre", field: "tipoSangre" },
-    { headerName: "Nacionalidad", field: "nacionalidad" },
-    // Agrega más columnas según sea necesario
-  ]);
+  const fields = [
+    { key: 'nombre', label: 'Nombre', _style: { width: '30%' } },
+    { key: 'puesto', label: 'Puesto', _style: { width: '20%' } },
+    { key: 'salario', label: 'Salario', _style: { width: '20%' } },
+    { key: 'estatus', label: 'Estatus', _style: { width: '20%' } },
+  ];
 
-  // Fetch data & update rowData state
-  useEffect(() => {
-    setRowData(empleados);
-  }, [empleados]);
+  const getBadge = (estatus) => {
+    switch (estatus) {
+      case 'Activo': return 'success';
+      case 'Inactivo': return 'danger';
+      default: return 'primary';
+    }
+  }
 
-  // Apply settings across all columns
-  const defaultColDef = useMemo(() => ({
-    filter: true,
-  }));
-
-  // Container: Defines the grid's theme & dimensions.
   return (
-    <div className={"ag-theme-quartz"} style={{ width: "100%", height: "100%" }}>
-      <AgGridReact
-        rowData={rowData}
-        columnDefs={colDefs}
-        defaultColDef={defaultColDef}
-        pagination={true}
+    <>
+      <CDataTable
+        items={empleados}
+        fields={fields}
+        hover
+        striped
+        bordered
+        size="sm"
+        itemsPerPage={10}
+        pagination
+        scopedSlots={{
+          'estatus':
+            (item) => (
+              <td>
+                <CBadge color={getBadge(item.estatus)}>
+                  {item.estatus}
+                </CBadge>
+              </td>
+            )
+        }}
       />
-    </div>
+    </>
   );
 };
 
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
