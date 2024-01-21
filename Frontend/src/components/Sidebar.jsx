1a9cb792328e7bcabbe505c662ed6fbb56b673d8
EdisonGualle <ed.gualle@gmail.com>
2024-01-21 00:06:52 -0500

    Actualizacion del Sidebar

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -2,26 +2,47 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 // Icons
 import {
-  RiBarChart2Line,
-  RiEarthLine,
   RiCustomerService2Line,
   RiCalendarTodoLine,
   RiLogoutCircleRLine,
   RiArrowRightSLine,
   RiMenu3Line,
   RiCloseLine,
+  RiUser3Line,
+  RiDashboardLine,
+  RiUserStarLine,
+  RiBriefcase2Line,
+  RiSettings3Line,
 } from "react-icons/ri";
 import useAuth from "../hooks/useAuth";
+
+import { GoChecklist } from "react-icons/go";
+import { LiaFileContractSolid } from "react-icons/lia";
+import { BsClipboard2Check } from "react-icons/bs";
+import { MdOutlineAssessment } from "react-icons/md";
+import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
 
 const Sidebar = () => {
   const { cerrarSesionAuth } = useAuth();
   const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
-  const [showSubmenu, setShowSubmenu] = useState(false);
   const cerrarSesion = () => {
     console.log("asd");
     cerrarSesionAuth();
   };
+
+  const [activeSubmenus, setActiveSubmenus] = useState([]);
+
+  const toggleSubMenu = (index) => {
+    setActiveSubmenus((prevSubmenus) => {
+      if (prevSubmenus.includes(index)) {
+        return prevSubmenus.filter((item) => item !== index);
+      } else {
+        return [...prevSubmenus, index];
+      }
+    });
+  };
+
   return (
     <>
       <div
@@ -30,8 +51,8 @@ const Sidebar = () => {
         } transition-all`}
       >
         <div>
-          <h1 className="text-center text-2xl font-bold text-black mb-10">
-            Admin<span className="text-primary text-4xl">.</span>
+          <h1 className="text-center text-2xl font-bold text-primary mb-10">
+            SIGDCOLTA<span className="text-secondary-100 text-4xl">.</span>
           </h1>
           <ul>
             <li>
@@ -39,89 +60,220 @@ const Sidebar = () => {
                 to="/"
                 className="flex text-secondary-100  items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100   hover:text-black transition-colors"
               >
-                <RiBarChart2Line className="text-primary" /> Dashboard
+                <RiDashboardLine className="text-primary" /> Dashboard
+              </Link>
+            </li>
+            {/* Mi Perfil */}
+            <li>
+              <Link
+                to="/tickets"
+                className="flex  text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100   hover:text-black transition-colors"
+              >
+                <RiUser3Line className="text-primary" /> Mi Perfil
+              </Link>
+            </li>
+            {/* Asistencia */}
+            <li>
+              <Link
+                to="/tickets"
+                className="flex  text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100   hover:text-black transition-colors"
+              >
+                <GoChecklist className="text-primary" /> Asistencia
+              </Link>
+            </li>
+            {/* Empleados */}
+            <li>
+              <Link
+                to="/empleados"
+                className="flex  text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100   hover:text-black transition-colors"
+              >
+                <RiUserStarLine className="text-primary" /> Empleados
+              </Link>
+            </li>
+            {/* Usuarios */}
+            <li>
+              <Link
+                to="/tickets"
+                className="flex  text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100   hover:text-black transition-colors"
+              >
+                <RiUser3Line className="text-primary" /> Usuarios
+              </Link>
+            </li>
+            {/*Contratos */}
+            <li>
+              <Link
+                to="/tickets"
+                className="flex  text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100   hover:text-black transition-colors"
+              >
+                <LiaFileContractSolid className="text-primary" /> Contratos
               </Link>
             </li>
 
+            {/* Capacitaciones */}
             <li>
               <Link
                 to="/tickets"
                 className="flex  text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100   hover:text-black transition-colors"
               >
-                <RiCustomerService2Line className="text-primary" /> Empleados
+                <RiBriefcase2Line className="text-primary" /> Capacitaciones
               </Link>
             </li>
+            {/* Permisos */}
+            <li>
+              <button
+                onClick={() => toggleSubMenu(4)}
+                className="w-full flex text-secondary-100 items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-100    hover:text-black transition-colors"
+              >
+                <span className="flex items-center gap-4">
+                  <BsClipboard2Check className="text-primary" /> Permisos
+                </span>
+                <RiArrowRightSLine
+                  className={`mt-1 ${
+                    activeSubmenus.includes(4) && "rotate-90"
+                  } transition-all`}
+                />
+              </button>
+              <ul
+                className={` ${
+                  activeSubmenus.includes(4) ? "h-[130px]" : "h-0"
+                } overflow-y-hidden transition-all`}
+              >
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2 text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Todos
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2   text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Por aprobar
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2  text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Solicitar permiso
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            {/* Horarios*/}
+            <li>
+              <button
+                onClick={() => toggleSubMenu(6)}
+                className="w-full flex text-secondary-100 items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-100    hover:text-black transition-colors"
+              >
+                <span className="flex items-center gap-4">
+                  <RiCalendarTodoLine className="text-primary" /> Horarios
+                </span>
+                <RiArrowRightSLine
+                  className={`mt-1 ${
+                    activeSubmenus.includes(6) && "rotate-90"
+                  } transition-all`}
+                />
+              </button>
+              <ul
+                className={` ${
+                  activeSubmenus.includes(6) ? "h-[100px]" : "h-0"
+                } overflow-y-hidden transition-all`}
+              >
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2 text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Calendario de actividades
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2   text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Configurar jornada
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            {/* Evaluaciones */}
             <li>
               <Link
                 to="/tickets"
                 className="flex  text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100   hover:text-black transition-colors"
               >
-                <RiCustomerService2Line className="text-primary" /> Permisos
+                <MdOutlineAssessment className="text-primary" /> Evaluaciones
               </Link>
             </li>
+            {/* Posicion laboral */}
+            <li>
+              <button
+                onClick={() => toggleSubMenu(8)}
+                className="w-full flex text-secondary-100 items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-100    hover:text-black transition-colors"
+              >
+                <span className="flex items-center gap-4">
+                  <HiOutlineBuildingOffice2 className="text-primary" /> Posicion
+                  laboral
+                </span>
+                <RiArrowRightSLine
+                  className={`mt-1 ${
+                    activeSubmenus.includes(8) && "rotate-90"
+                  } transition-all`}
+                />
+              </button>
+              <ul
+                className={` ${
+                  activeSubmenus.includes(8) ? "h-[130px]" : "h-0"
+                } overflow-y-hidden transition-all`}
+              >
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2 text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Direcciones
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2   text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Unidades
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2   text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
+                  >
+                    Cargos
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            {/* Configuraciones */}
             <li>
               <Link
                 to="/"
-                className="flex text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100 hover:text-black transition-colors"
+                className="flex  text-secondary-100 items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100   hover:text-black  transition-colors"
               >
-                <RiCalendarTodoLine className="text-primary" /> Calendario
+                <span className="text-primary hover:text-yellow-900">
+                  <RiSettings3Line />
+                </span>{" "}
+                Configuraciones
               </Link>
-              <li>
-                <button
-                  onClick={() => setShowSubmenu(!showSubmenu)}
-                  className="w-full flex text-secondary-100 items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-100    hover:text-black transition-colors"
-                >
-                  <span className="flex items-center gap-4">
-                    <RiEarthLine className="text-primary" /> Configuracion
-                  </span>
-                  <RiArrowRightSLine
-                    className={`mt-1 ${
-                      showSubmenu && "rotate-90"
-                    } transition-all`}
-                  />
-                </button>
-                <ul
-                  className={` ${
-                    showSubmenu ? "h-[130px]" : "h-0"
-                  } overflow-y-hidden transition-all`}
-                >
-                  <li>
-                    <Link
-                      to="/"
-                      className="py-2 text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
-                    >
-                      Direcciones
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="py-2   text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
-                    >
-                      Unidades
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/"
-                      className="py-2  text-secondary-100 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-primary transition-colors"
-                    >
-                      Cargos
-                    </Link>
-                  </li>
-                </ul>
-              </li>
             </li>
           </ul>
         </div>
         <nav>
-          {/* <Link
-            to="/"
-            className="flex  text-secondary-100   hover:text-black items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100 transition-colors"
-          >
-            <RiLogoutCircleRLine className="text-primary" /> Cerrar sesión
-          </Link> */}
-
           <button
             onClick={cerrarSesion}
             className="flex  text-secondary-100   hover:text-black items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-100 transition-colors"
