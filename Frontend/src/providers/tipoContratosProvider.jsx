ac024bd17f832ec52a8b01d75e5d4fe11f70cedb
EdisonGualle <ed.gualle@gmail.com>
2024-01-30 18:41:52 -0500

    Control de version.1.

--- Frontend/src/providers/tipoContratosProvider.jsx
+++ Frontend/src/providers/tipoContratosProvider.jsx
@@ -1,22 +1,25 @@
-import { createContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { createContext, useState, useEffect } from "react";
 import clienteAxios from "../config/clienteAxios";
-
+import useAuth from "../hooks/useAuth";
 const TiposContratoContext = createContext();
 
 const TiposContratoProvider = ({ children }) => {
   const [tiposContrato, setTiposContrato] = useState([]);
   const [cargando, setCargando] = useState(false);
+  const [alerta, setAlerta] = useState({});
+  const [tipoContrato, setTipoContrato] = useState({});
+  // Puedes agregar más estados según tus necesidades
+
   const navigate = useNavigate();
-  
-  
+  const { auth } = useAuth();
+
   useEffect(() => {
     const obtenerTiposContrato = async () => {
       try {
         const token = localStorage.getItem("token");
         if (!token) {
-          setCargando(false);
-          return navigate("/");;
+          return;
         }
 
         const config = {
@@ -27,7 +30,8 @@ const TiposContratoProvider = ({ children }) => {
         };
 
         const { data } = await clienteAxios("/contratos-tipo", config);
-        setTiposContrato(data.data); // Intenta acceder a la propiedad correcta
+
+       setTiposContrato(data.data); // Intenta acceder a la propiedad correcta
 
       } catch (error) {
         console.error("Error al cargar los datos:", error);
@@ -37,7 +41,7 @@ const TiposContratoProvider = ({ children }) => {
     };
 
     obtenerTiposContrato();
-  }, [auth]);
+  }, []);
 
   const contextValue = {
     tiposContrato,
